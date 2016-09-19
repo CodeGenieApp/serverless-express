64568d5c5fd8d2e4456cfd3288132f96bd874b0a
Andrews <brett.j.andrews@gmail.com>
2016-09-19 11:32:43 -0700

    set region in config instead of using default profile

--- example/configure.js
+++ example/configure.js
@@ -5,6 +5,7 @@ const exec = require('child_process').exec
 const args = process.argv.slice(2)
 const accountId = args[0]
 const bucketName = args[1]
+const region = args[2] || 'us-east-1'
 
 if (!accountId || accountId.length !== 12) {
     console.error('You must supply a 12 digit account id as the first argument')
@@ -16,24 +17,25 @@ if (!bucketName) {
     return
 }
 
-exec('aws configure get region', (err, stdout, stderr) => {
-    if (err) {
-        console.log(err)
-        return
-    }
+if (!region.test(/.*-.*-[0-9]*/)) {
+    console.warn('The supplied region does not match the expected pattern, eg. us-east-1')
+}
 
-    if (stderr) {
-        console.log(stderr)
-        return
-    }
+modifyCloudFormationFile()
+modifySimpleProxyFile()
+modifyPackageFile()
 
-    const region = stdout.match(/.*-.*-[0-9]*/)[0]// Command output has a newline
+function modifyCloudFormationFile() {
+    const cloudFormationPath = './cloudformation.json'
+    const cloudFormation = fs.readFileSync(cloudFormationPath, 'utf8')
+    const cloudFormationModified = cloudFormation
+        .replace(/YOUR_ACCOUNT_ID/g, accountId)
+        .replace(/YOUR_AWS_REGION/g, region)
 
-    modifySimpleProxyFile(region)
-    modifyPackageFile(region)
-})
+    fs.writeFileSync(cloudFormationPath, cloudFormationModified, 'utf8')
+}
 
-function modifySimpleProxyFile(region) {
+function modifySimpleProxyFile() {
     const simpleProxyApiPath = './simple-proxy-api.yaml'
     const simpleProxyApi = fs.readFileSync(simpleProxyApiPath, 'utf8')
     const simpleProxyApiModified = simpleProxyApi
@@ -43,10 +45,12 @@ function modifySimpleProxyFile(region) {
     fs.writeFileSync(simpleProxyApiPath, simpleProxyApiModified, 'utf8')
 }
 
-function modifyPackageFile(region) {
+function modifyPackageFile() {
     const packageJsonPath = './package.json'
     const packageJson = fs.readFileSync(packageJsonPath, 'utf8')
-    const packageJsonModified = packageJson.replace(/YOUR_UNIQUE_BUCKET_NAME/g, bucketName)
+    const packageJsonModified = packageJson
+        .replace(/YOUR_UNIQUE_BUCKET_NAME/g, bucketName)
+        .replace(/YOUR_AWS_REGION/g, region)
 
     fs.writeFileSync(packageJsonPath, packageJsonModified, 'utf8')
 }
