4e6f67674d553f38fb3162f93745e5c979709685
Andrews <brett.j.andrews@gmail.com>
2016-09-26 15:16:44 -0700

    express-server.js => app.js

--- example/app.js
+++ example/app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
 
-app.use(cors());
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
