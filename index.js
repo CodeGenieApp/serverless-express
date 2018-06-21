462bfdfad60ab813a90faca0c6c64521aa716d55
Alex Brooke <akb.sbc@gmail.com>
2018-06-21 15:11:04 -0500

    forcibly inject Content-Length into event.headers
    - only if body is present and there are no headers
    - seems hacky, but ensures that the `x-apigateway-event` header is set
    properly(?) later (i.e. including the content-length field)

--- index.js
+++ index.js
@@ -33,13 +33,16 @@ function isContentTypeBinaryMimeType(params) {
 
 function mapApiGatewayEventToHttpRequest(event, context, socketPath) {
     const headers = event.headers || {} // NOTE: Mutating event.headers; prefer deep clone of event.headers
+    // NOTE: API Gateway is not setting Content-Length header on any requests even when they have a body  
+    if (event.body && !headers['Content-Length']) {
+        // set header on event directly so x-apigateway-event is set correctly later
+        if (!event.headers) event.headers = {}
+        event.headers['Content-Length'] = Buffer.byteLength(event.body)
+        headers['Content-Length'] = Buffer.byteLength(event.body)
+    }
     const eventWithoutBody = Object.assign({}, event)
     delete eventWithoutBody.body
 
-    // NOTE: API Gateway is not setting Content-Length header on any requests even when they have a body  
-    if (event.body && !headers['Content-Length']) {
-      headers['Content-Length'] = Buffer.byteLength(event.body)
-    }
     headers['x-apigateway-event'] = encodeURIComponent(JSON.stringify(eventWithoutBody))
     headers['x-apigateway-context'] = encodeURIComponent(JSON.stringify(context))
 
