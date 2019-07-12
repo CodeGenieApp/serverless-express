0cafd69a325a16a211336d4459f77a0153f9e5a6
Brett Andrews <brett.j.andrews@gmail.com>
2019-07-12 08:27:52 -0700

    docs: add koa example

---
+++ examples/basic-starter-koa/scripts/local.js
@@ -0,0 +1,31 @@
+const lambdaFunction = require('../src/lambda.js')
+const apiGatewayEvent = require('../api-gateway-event.json')
+
+const context = {
+  succeed: v => {
+    console.info(v)
+    process.exit(0)
+  }
+}
+const callback = (e, v) => {
+  if (e) console.error(e)
+  if (v) console.info(v)
+  process.exit(0)
+}
+const server = lambdaFunction.handler(apiGatewayEvent, context, callback)
+
+process.stdin.resume()
+
+function exitHandler (options, err) {
+  if (options.cleanup && server && server.close) {
+    server.close()
+  }
+
+  if (err) console.error(err.stack)
+  if (options.exit) process.exit()
+}
+
+process.on('exit', exitHandler.bind(null, { cleanup: true }))
+process.on('SIGINT', exitHandler.bind(null, { exit: true })) // ctrl+c event
+process.on('SIGTSTP', exitHandler.bind(null, { exit: true })) // ctrl+v event
+process.on('uncaughtException', exitHandler.bind(null, { exit: true }))
---
+++ examples/basic-starter-koa/src/app.js
@@ -0,0 +1,15 @@
+const Koa = require('koa')
+const Router = require('koa-router')
+
+const app = new Koa()
+const router = new Router()
+
+router.get('/', (ctx, next) => {
+  ctx.body = 'Hi~'
+})
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
+
+module.exports = app.callback()
