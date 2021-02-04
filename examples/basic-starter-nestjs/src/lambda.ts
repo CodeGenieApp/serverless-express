812b36ee8ad618e331001bed5f5611fce87416fa
Michael Merrill <michael@michaelmerrill.me>
2021-02-04 16:04:29 -0700

    docs(nestjs example): add nestjs example
    re #350

---
+++ examples/basic-starter-nestjs/src/lambda.ts
@@ -0,0 +1,32 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import serverlessExpress from '@vendia/serverless-express';
+import { Context, Handler } from 'aws-lambda';
+import express from 'express';
+
+import { AppModule } from './app.module';
+
+let cachedServer: Handler;
+
+async function bootstrap() {
+  if (!cachedServer) {
+    const expressApp = express();
+    const nestApp = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(expressApp),
+    );
+
+    nestApp.enableCors();
+
+    await nestApp.init();
+
+    cachedServer = serverlessExpress({ app: expressApp }).handler;
+  }
+
+  return cachedServer;
+}
+
+export const handler = async (event: any, context: Context, callback: any) => {
+  const server = await bootstrap();
+  return server(event, context, callback);
+};
