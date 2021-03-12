abd1ba63d1064cbd25f3fd53ea0badf69653e150
Christian Hoffmann <352753+hffmnn@users.noreply.github.co>
2021-03-12 09:37:06 +0100

    fix: add unit test

---
+++ __tests__/unit.api-gateway-v2.js
@@ -0,0 +1,21 @@
+const eventSources = require('../src/event-sources')
+const testUtils = require('../src/event-sources/utils.test')
+
+const apiGatewayEventSource = eventSources.getEventSource({
+  eventSourceName: 'AWS_API_GATEWAY_V2'
+})
+
+test('request has correct headers', () => {
+  const req = getReq()
+  // see https://github.com/vendia/serverless-express/issues/387
+  expect(typeof req).toEqual('object')
+  expect(JSON.stringify(req.headers)).toEqual(
+    '{"cookie":"","host":"localhost:9000","user-agent":"curl/7.64.1","accept":"*/*","x-forwarded-proto":"http","x-forwarded-port":"9000"}'
+  )
+})
+
+function getReq () {
+  const event = testUtils.sam_httpapi_event
+  const request = apiGatewayEventSource.getRequest({ event })
+  return request
+}
