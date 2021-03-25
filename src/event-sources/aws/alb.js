91a87902b31878341bab417075d65b00970ab8ad
Frederic Germain <frederic.germain@gmail.com>
2021-03-25 17:17:41 +0100

    fix: auto-unescape query parameters on ALB (#219, #241)

--- src/event-sources/aws/alb.js
+++ src/event-sources/aws/alb.js
@@ -1,6 +1,42 @@
+const url = require('url')
 const { getRequestValuesFromEvent, getMultiValueHeaders } = require('../utils')
 
-const getRequestValuesFromAlbEvent = ({ event }) => getRequestValuesFromEvent({ event })
+function getPathWithQueryStringUseUnescapeParams ({
+  event,
+  // NOTE: Use `event.pathParameters.proxy` if available ({proxy+}); fall back to `event.path`
+  path = (event.pathParameters && event.pathParameters.proxy && `/${event.pathParameters.proxy}`) || event.path,
+  // NOTE: Strip base path for custom domains
+  stripBasePath = '',
+  replaceRegex = new RegExp(`^${stripBasePath}`)
+}) {
+  const query = {}
+  // decode everything back into utf-8 text.
+  if (event.multiValueQueryStringParameters) {
+    for (const key in event.multiValueQueryStringParameters) {
+      const formattedKey = decodeURIComponent(key)
+      query[formattedKey] = event.multiValueQueryStringParameters[key].map(value => decodeURIComponent(value))
+    }
+  } else {
+    for (const key in event.queryStringParameters) {
+      const formattedKey = decodeURIComponent(key)
+      query[formattedKey] = decodeURIComponent(event.queryStringParameters[key])
+    }
+  }
+
+  return url.format({
+    pathname: path.replace(replaceRegex, ''),
+    query
+  })
+}
+
+const getRequestValuesFromAlbEvent = ({ event }) => {
+  const values = getRequestValuesFromEvent({
+    event,
+    path: getPathWithQueryStringUseUnescapeParams({ event })
+  })
+  return values
+}
+
 const getResponseToAlb = ({
   statusCode,
   body,
