093af02839d36be0806c7113eac83208590c9e2b
Kenneth Wußmann <kenneth@wussmann.net>
2022-01-23 04:02:45 +0100

    fix(types): add missing config params (#486) (#488)

--- src/configure.d.ts
+++ src/configure.d.ts
@@ -1,6 +1,7 @@
 import { RequestListener } from "http";
 import { Handler } from "aws-lambda";
 import Logger from "./logger";
+import Framework from "./frameworks";
 
 type EventSources = "AWS_SNS" | "AWS_DYNAMODB";
 
@@ -18,12 +19,23 @@ interface BinarySettings {
   isBinary?: Function | boolean;
   contentTypes?: string[];
 }
+
+interface LogSettings {
+  level: string;
+}
+
 interface ConfigureParams {
   app: RequestListener;
+  logSettings?: LogSettings;
+  log?: Logger;
+  framework?: Framework;
   binaryMimeTypes?: string[];
   binarySettings?: BinarySettings;
+  resolutionMode?: string;
+  eventSourceName?: string;
   eventSource?: EventSource; // TODO:
   eventSourceRoutes?: { [key in EventSources]?: string };
+  respondWithErrors?: boolean;
 }
 
 interface BinarySettings {
@@ -37,7 +49,9 @@ interface ConfigureResult<TEvent = any, TResult = any> {
   proxy: (proxyParams: ProxyParams) => Promise<Object>;
 }
 
-declare function configure<TEvent = any, TResult = any>(configureParams: ConfigureParams): Handler<TEvent, TResult> & ConfigureResult<TEvent, TResult>;
+declare function configure<TEvent = any, TResult = any>(
+  configureParams: ConfigureParams
+): Handler<TEvent, TResult> & ConfigureResult<TEvent, TResult>;
 
 // declare function proxy(proxyParams: ProxyParams): Promise<any>
 
