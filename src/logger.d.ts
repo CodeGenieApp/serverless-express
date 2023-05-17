f4c9ead828fea9aceed0007088cbd17aca724518
Vinícius Lourenço <vinyygamerlol@gmail.com>
2023-05-17 11:26:25 -0300

    fix(types): better types for logger

--- src/logger.d.ts
+++ src/logger.d.ts
@@ -1,7 +1,12 @@
-interface Logger {
-  info: (message: string, additional: Object) => void
-  debug: (message: string, additional: Object) => void
-  error: (message: string, additional: Object) => void
+export interface Logger {
+  info: (message: string, ...additional: Object[]) => void;
+  debug: (message: string, ...additional: Object[]) => void;
+  error: (message: string, ...additional: Object[]) => void;
+  warn: (message: string, ...additional: Object[]) => void;
+  verbose: (message: string, ...additional: Object[]) => void;
+}
+interface Options {
+  level?: 'error' | 'warn' | 'info' | 'verbose' | 'debug' | string;
 }
 
-export default Logger
+export default function logger(options?: Options): Logger;
