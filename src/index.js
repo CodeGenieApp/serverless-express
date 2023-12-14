9f4a3c6e0b05f618b55d060ac06861261336e3d2
Brett <brett.j.andrews@gmail.com>
2023-12-14 22:49:46 +1100

    proxy through to @codegenie/serverless-express

--- src/index.js
+++ src/index.js
@@ -1,38 +1,7 @@
-const configure = require('./configure')
-const { getCurrentInvoke } = require('./current-invoke')
+const codeGenieServerlessExpress = require('@codegenie/serverless-express')
 
-module.exports = configure
-module.exports.getCurrentInvoke = getCurrentInvoke
-module.exports.configure = configure
-
-// Legacy/deprecated:
-
-function createServer (app, serverListenCallback, binaryMimeTypes) {
-  console.warn('[DEPRECATION NOTICE] You\'re using the deprecated createServer method that will be removed in the next major version. See https://github.com/vendia/serverless-express/blob/mainline/UPGRADE.md to upgrade.')
-
-  if (serverListenCallback) {
-    throw new Error('serverListenCallback is no longer supported.')
-  }
-
-  const configureOptions = {
-    app,
-    binarySettings: {
-      contentTypes: binaryMimeTypes
-    }
-  }
-
-  return configureOptions
-}
-
-function proxy (configureOptions, event, context, resolutionMode, callback) {
-  console.warn('[DEPRECATION NOTICE] You\'re using the deprecated proxy method that will be removed in the next major version. See https://github.com/vendia/serverless-express/blob/mainline/UPGRADE.md to upgrade.')
-
-  const se = configure({
-    ...configureOptions,
-    resolutionMode
-  })
-  return se(event, context, callback)
-}
-
-module.exports.createServer = createServer
-module.exports.proxy = proxy
+module.exports = codeGenieServerlessExpress.configure
+module.exports.getCurrentInvoke = codeGenieServerlessExpress.getCurrentInvoke
+module.exports.configure = codeGenieServerlessExpress.configure
+module.exports.createServer = codeGenieServerlessExpress.createServer
+module.exports.proxy = codeGenieServerlessExpress.proxy
--- src/middleware.js
+++ src/middleware.js
@@ -1,21 +1,3 @@
-const { getCurrentInvoke } = require('./current-invoke')
+const codeGenieServerlessExpressMiddleware = require('@codegenie/serverless-express/src/middleware')
 
-module.exports.eventContext = options => function apiGatewayEventParser (req, res, next) {
-  options = options || {}
-  const reqPropKey = options.reqPropKey || 'apiGateway'
-  const deleteHeaders = options.deleteHeaders === undefined ? true : options.deleteHeaders
-
-  const currentInvoke = getCurrentInvoke()
-
-  req[reqPropKey] = currentInvoke
-
-  if (!deleteHeaders) {
-    const clonedEventWithoutBody = JSON.parse(JSON.stringify(currentInvoke.event))
-    delete clonedEventWithoutBody.body
-
-    req.headers['x-apigateway-event'] = encodeURIComponent(JSON.stringify(clonedEventWithoutBody))
-    req.headers['x-apigateway-context'] = encodeURIComponent(JSON.stringify(currentInvoke.context))
-  }
-
-  next()
-}
+module.exports.eventContext = codeGenieServerlessExpressMiddleware.eventContext
