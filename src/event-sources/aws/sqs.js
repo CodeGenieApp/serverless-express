1d94a8fef89d710a279e25fb3e67a95d03dd3415
Do Minh Hai <1484298+dominhhai@users.noreply.github.com>
2023-12-21 13:22:28 +0700

    fix coding convention style

--- src/event-sources/aws/sqs.js
+++ src/event-sources/aws/sqs.js
@@ -1,24 +1,24 @@
 const getRequestValuesFromSqs = ({ event }) => {
-  const method = "POST";
-  const headers = { host: "sqs.amazonaws.com" };
-  const body = event;
+  const method = 'POST'
+  const headers = { host: 'sqs.amazonaws.com' }
+  const body = event
 
   return {
     method,
     headers,
     body,
-  };
-};
+  }
+}
 
 const getResponseToSqs = ({ statusCode, body }) => {
   if (statusCode >= 400) {
-    throw new Error(body);
+    throw new Error(body)
   }
-  
-  return JSON.parse(body);
-};
+
+  return JSON.parse(body)
+}
 
 module.exports = {
   getRequest: getRequestValuesFromSqs,
   getResponse: getResponseToSqs,
-};
+}
--- src/event-sources/aws/step-functions.js
+++ src/event-sources/aws/step-functions.js
@@ -1,14 +1,14 @@
 const getRequestValuesFromStepFunctions = ({ event }) => {
-  const method = "POST";
-  const headers = { host: "stepfunctions.amazonaws.com" };
-  const body = event;
+  const method = 'POST'
+  const headers = { host: 'stepfunctions.amazonaws.com' }
+  const body = event
 
   return {
     method,
     headers,
     body,
-  };
-};
+  }
+}
 
 function getResponseToStepFunctions({
   statusCode,
@@ -16,17 +16,17 @@ function getResponseToStepFunctions({
   isBase64Encoded = false,
 }) {
   if (statusCode >= 400) {
-    throw new Error(body);
+    throw new Error(body)
   }
 
   if (isBase64Encoded) {
-    throw new Error("base64 encoding is not supported");
+    throw new Error('base64 encoding is not supported')
   }
 
-  return JSON.parse(body);
+  return JSON.parse(body)
 }
 
 module.exports = {
   getRequest: getRequestValuesFromStepFunctions,
   getResponse: getResponseToStepFunctions,
-};
+}
