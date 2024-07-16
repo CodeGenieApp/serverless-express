bef14dcc33197d17671ee50977d5aadc3d862ae9
Michael Harrington <kibiz0r@gmail.com>
2024-07-16 12:39:05 -0400

    refactor: slight optimization since we know the size of the 2D headers array

--- src/request.js
+++ src/request.js
@@ -17,7 +17,12 @@ module.exports = class ServerlessRequest extends http.IncomingMessage {
 
     // IncomingMessage has a lot of logic for when to lowercase or alias well-known header names,
     // so we delegate to that logic here
-    const rawHeaders = Object.entries(headers).flat()
+    const headerEntries = Object.entries(headers)
+    const rawHeaders = new Array(headerEntries.length * 2)
+    for (let i = 0; i < headerEntries.length; i++) {
+      rawHeaders[i * 2] = headerEntries[i][0]
+      rawHeaders[i * 2 + 1] = headerEntries[i][1]
+    }
     this._addHeaderLines(rawHeaders, rawHeaders.length)
 
     Object.assign(this, {
