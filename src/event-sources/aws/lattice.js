c354904f3bea3bd18848371a4e2215f1705ec8c6
Christoph Windpassinger <extern.christoph.windpassinger@vwfs.io>
2025-05-12 12:43:05 +0200

    Logic analog to ALB

--- src/event-sources/aws/lattice.js
+++ src/event-sources/aws/lattice.js
@@ -1,53 +1,68 @@
 const url = require('url');
-const { getEventBody, getCommaDelimitedHeaders } = require('../utils');
+const { getRequestValuesFromEvent, getMultiValueHeaders } = require('../utils');
 
-function getRequestValuesFromLatticeEvent({ event }) {
-  const { httpMethod, path, queryStringParameters, headers, body, isBase64Encoded } = event;
-
-  const formattedPath = url.format({
-    pathname: path,
-    query: queryStringParameters,
-  });
-
-  const parsedHeaders = {};
-  Object.entries(headers || {}).forEach(([key, value]) => {
-    parsedHeaders[key.toLowerCase()] = value;
-  });
-
-  let parsedBody;
-  if (body) {
-    parsedBody = getEventBody({ event });
-    parsedHeaders['content-length'] = Buffer.byteLength(parsedBody, isBase64Encoded ? 'base64' : 'utf8');
+function getPathWithQueryStringUseUnescapeParams ({
+  event,
+  path = event.path,
+  // NOTE: Strip base path for custom domains
+  stripBasePath = '',
+  replaceRegex = new RegExp(`^${stripBasePath}`)
+}) {
+  const query = {}
+  // decode everything back into utf-8 text.
+  if (event.multiValueQueryStringParameters) {
+    for (const key in event.multiValueQueryStringParameters) {
+      const formattedKey = decodeUrlencoded(key)
+      query[formattedKey] = event.multiValueQueryStringParameters[key].map(value => decodeUrlencoded(value))
+    }
+  } else {
+    for (const key in event.queryStringParameters) {
+      const formattedKey = decodeUrlencoded(key)
+      query[formattedKey] = decodeUrlencoded(event.queryStringParameters[key])
+    }
   }
 
-  console.log('HTTP Method:', httpMethod);
-  console.log('Path:', path);
-  console.log('Formatted Path:', formattedPath);
-  console.log('Headers:', parsedHeaders);
-  console.log('Body:', parsedBody);
-
-  return {
-    method: httpMethod,
-    headers: parsedHeaders,
-    body: parsedBody,
-    remoteAddress: event.requestContext?.http?.sourceIp || '',
-    path: formattedPath,
-  };
+  return url.format({
+    pathname: path.replace(replaceRegex, ''),
+    query
+  })
 }
 
-function getResponseToLattice({ statusCode, body, headers = {}, isBase64Encoded = false }) {
-  if (headers['transfer-encoding'] === 'chunked') {
-    throw new Error('chunked encoding is not supported by AWS VPC Lattice');
-  }
+// Decode an "application/x-www-form-urlencoded" encoded string.
+function decodeUrlencoded (val) {
+  return decodeURIComponent(val.replace(/\+/g, '%20'))
+}
 
-  const responseToLattice = {
+const getRequestValuesFromLatticeEvent = ({ event }) => {
+  const values = getRequestValuesFromEvent({
+    event,
+    path: getPathWithQueryStringUseUnescapeParams({ event })
+  })
+  return values
+}
+
+const getResponseToLattice = ({
+  event,
+  statusCode,
+  body,
+  headers: responseHeaders,
+  isBase64Encoded
+}) => {
+  const multiValueHeaders = !event.headers ? getMultiValueHeaders({ headers: responseHeaders }) : undefined
+  const headers = event.headers
+    ? Object.entries(responseHeaders).reduce((acc, [k, v]) => {
+      acc[k] = Array.isArray(v) ? v[0] : v
+      return acc
+    }, {})
+    : undefined
+
+  return {
     statusCode,
     body,
-    isBase64Encoded,
-    headers: getCommaDelimitedHeaders({ headersMap: headers }),
-  };
-
-  return responseToLattice;
+    headers,
+    multiValueHeaders,
+    isBase64Encoded
+  }
 }
 
 module.exports = {
