3f802befde7cd058f762e177e7d7a0d6e5a87f33
Christoph Windpassinger <extern.christoph.windpassinger@vwfs.io>
2025-05-12 13:30:22 +0200

    Logs

--- src/event-sources/aws/lattice.js
+++ src/event-sources/aws/lattice.js
@@ -38,6 +38,8 @@ const getRequestValuesFromLatticeEvent = ({ event }) => {
     event,
     path: getPathWithQueryStringUseUnescapeParams({ event })
   })
+
+  console.log('getRequestValuesFromLatticeEvent', values);
   return values
 }
 
