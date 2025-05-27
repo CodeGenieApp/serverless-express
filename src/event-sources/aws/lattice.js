efdf3a78362c5f66185b779461cd96e36d899073
Christoph Windpassinger <extern.christoph.windpassinger@vwfs.io>
2025-05-27 16:25:58 +0200

    feat: another try

--- src/event-sources/aws/lattice.js
+++ src/event-sources/aws/lattice.js
@@ -1,36 +1,36 @@
-const url = require('url')
+// const url = require('url')
 const { getRequestValuesFromEvent, getCommaDelimitedHeaders } = require('../utils')
 
-function getPathWithQueryStringUseUnescapeParams ({
-  event,
-  path = event.path,
-  // NOTE: Strip base path for custom domains
-  stripBasePath = '',
-  replaceRegex = new RegExp(`^${stripBasePath}`)
-}) {
-  const query = {}
-  // decode everything back into utf-8 text.
-  for (const key in event.queryStringParameters) {
-    const formattedKey = decodeUrlencoded(key)
-    query[formattedKey] = event.queryStringParameters[key].map(value => decodeUrlencoded(value))
-  }
+// function getPathWithQueryStringUseUnescapeParams ({
+//   event,
+//   path = event.path,
+//   // NOTE: Strip base path for custom domains
+//   stripBasePath = '',
+//   replaceRegex = new RegExp(`^${stripBasePath}`)
+// }) {
+//   const query = {}
+//   // decode everything back into utf-8 text.
+//   for (const key in event.queryStringParameters) {
+//     const formattedKey = decodeUrlencoded(key)
+//     query[formattedKey] = event.queryStringParameters[key].map(value => decodeUrlencoded(value))
+//   }
 
-  return url.format({
-    pathname: path.replace(replaceRegex, '')
-    // query
-  })
-}
+//   return url.format({
+//     pathname: path.replace(replaceRegex, ''),
+//     query
+//   })
+// }
 
-// Decode an "application/x-www-form-urlencoded" encoded string.
-function decodeUrlencoded (val) {
-  return decodeURIComponent(val.replace(/\+/g, '%20'))
-}
+// // Decode an "application/x-www-form-urlencoded" encoded string.
+// function decodeUrlencoded (val) {
+//   return decodeURIComponent(val.replace(/\+/g, '%20'))
+// }
 
 const getRequestValuesFromLatticeEvent = ({ event }) => {
   const values = getRequestValuesFromEvent({
     event,
     method: event.method,
-    path: getPathWithQueryStringUseUnescapeParams({ event })
+    path: event.path // getPathWithQueryStringUseUnescapeParams({ event })
   })
 
   // NOTE: Lattice always sends the headers as array that needs to be converted to a comma delimited string
