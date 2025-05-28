a0b510cad14dfe291f182faaa5e9e09f5f57360c
Christoph Windpassinger <extern.christoph.windpassinger@vwfs.io>
2025-05-28 09:07:05 +0200

    feat: cleanup

--- src/event-sources/aws/lattice.js
+++ src/event-sources/aws/lattice.js
@@ -1,39 +1,13 @@
-// const url = require('url')
 const { getRequestValuesFromEvent, getCommaDelimitedHeaders } = require('../utils')
-
-// function getPathWithQueryStringUseUnescapeParams ({
-//   event,
-//   path = event.path,
-//   // NOTE: Strip base path for custom domains
-//   stripBasePath = '',
-//   replaceRegex = new RegExp(`^${stripBasePath}`)
-// }) {
-//   const query = {}
-//   // decode everything back into utf-8 text.
-//   for (const key in event.queryStringParameters) {
-//     const formattedKey = decodeUrlencoded(key)
-//     query[formattedKey] = event.queryStringParameters[key].map(value => decodeUrlencoded(value))
-//   }
-
-//   return url.format({
-//     pathname: path.replace(replaceRegex, ''),
-//     query
-//   })
-// }
-
-// // Decode an "application/x-www-form-urlencoded" encoded string.
-// function decodeUrlencoded (val) {
-//   return decodeURIComponent(val.replace(/\+/g, '%20'))
-// }
 
 const getRequestValuesFromLatticeEvent = ({ event }) => {
   const values = getRequestValuesFromEvent({
     event,
     method: event.method,
-    path: event.path // getPathWithQueryStringUseUnescapeParams({ event })
+    path: event.path // query parameters are already included in the path
   })
 
-  // NOTE: Lattice always sends the headers as array that needs to be converted to a comma delimited string
+  // Lattice always sends the headers as array that needs to be converted to a comma delimited string
   values.headers = getCommaDelimitedHeaders({ headersMap: event.headers, lowerCaseKey: true })
 
   return values
